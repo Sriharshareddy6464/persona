39746d337459075d2f2981b04834272909c96852
Sriharshareddy6464 <adapalasriharshareddy@gmail.com>
2026-05-18 11:26:49 +0530

    chore : add pdf ingestion pipeline

---
+++ src/service/pdf_service.py
@@ -0,0 +1,38 @@
+from pypdf import PdfReader
+
+def extract_pdf_text(file_path: str):
+
+    try:
+
+        reader = PdfReader(file_path)
+
+        pages=len(reader.pages)
+
+        text=[]
+
+        for page in reader.pages:
+
+            content=page.extract_text()
+
+            if content:
+                text.append(content)
+
+        final_text="\n".join(text)
+
+        return {
+
+            "success":True,
+            "pages":pages,
+            "characters":len(final_text),
+            "text":final_text
+
+        }
+
+    except Exception as e:
+
+        return {
+
+            "success":False,
+            "error":str(e)
+
+        }
